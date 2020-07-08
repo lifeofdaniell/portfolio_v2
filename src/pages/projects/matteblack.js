cc36eda3396f7e26a5274b87976f981a75474c5a
lifeofdaniell <areoladanielopeyemi@gmail.com>
2020-07-08 10:43:20 +0100

    added portraits

--- src/pages/projects/matteblack.js
+++ src/pages/projects/matteblack.js
@@ -2,6 +2,8 @@ import React from "react";
 import HeaderPage from "../../components/header";
 import Template from "../../components/Template";
 import SEO from "../../components/seo";
+import Footer from "../../components/footer";
+import ImageGrid from "../../components/images";
 
 const Matteblack = () => (
   <div>
@@ -19,6 +21,21 @@ const Matteblack = () => (
       description2="This series of images showcases how boldly the dark skin stands out from any white material, surface or fabric. "
       description3="RAW images from Canon EOS200D with the EF-S18-55mm f/3.5-5.6 III and EF50mm f/1.8STM lens."
     />
+
+    <ImageGrid
+      image1="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589735399/adekemi4_mc9gda.jpg"
+      image2="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402425/amos2_m5mzd5.jpg"
+      image3="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402336/adekemi5_u31mgb.jpg"
+      image5="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402398/adekemi_2_lhdgpt.jpg"
+      image6="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589735107/adekemi3_oxahik.jpg"
+      image9="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402394/amos6_xpkc23.jpg"
+      image10="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402406/adekemi6_zuzxbh.jpg"
+      image11="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402346/adekemi7_y1plga.jpg"
+      image13="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402371/amos4_c4ugxh.jpg"
+      image14="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402311/0003_ywezoo.jpg"
+      image15="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589402363/amos3_px9z8n.jpg"
+    />
+    <Footer />
   </div>
 );
 
--- src/pages/projects/portraits.js
+++ src/pages/projects/portraits.js
@@ -21,7 +21,7 @@ const Portraits = () => (
       description2="RAW images from Canon EOS200D with the EF-S18-55mm f/3.5-5.6 III and EF50mm f/1.8STM lens."
     />
     <ImageGrid
-      imgae27="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403305/DALU_qik4cm.jpg"
+      image1="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589732523/IMG_4017-2_bts4im.jpg"
       image2="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403327/fam_11_of_28_twktdu.jpg"
       image3="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403343/fam_15_of_28_tcqwrx.jpg"
       image4="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403363/fam_22_of_28_lfojgc.jpg"
@@ -30,6 +30,7 @@ const Portraits = () => (
       image7="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403408/GRACE_enufjf.jpg"
       image8="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403522/viconco-0863_oqo4ig.jpg"
       image9="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403493/nkechi_aqtm9v.jpg"
+      image10="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403396/FEYIYEMISIcover_zr3eyb.jpg"
       image11="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403466/leon-2_i1jpdn.jpg"
       image12="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403490/leon-4_rsgazm.jpg"
       image13="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403475/leon-7_atfepw.jpg"
@@ -39,20 +40,20 @@ const Portraits = () => (
       image17="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403377/FEYIYEMISI-6_x3c2cw.jpg"
       image18="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589732153/FEYIYEMISI-8_onjtip.jpg"
       image19="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403389/FEYIYEMISI-9_x75xbe.jpg"
-      image10="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403396/FEYIYEMISIcover_zr3eyb.jpg"
+      image20="https://res.cloudinary.com/areoladaniel-com/image/upload/v1593794284/GatsbyPortfolio/optimized/cover6_d3mwbw.jpg"
       image21="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403537/soundz_1_of_3_sybtva.jpg"
       image22="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403545/soundz_2_of_3_z0ztdx.jpg"
       image23="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589732276/LEONCOVER_ceczw3.jpg"
       image24="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403421/IMG_0550-1_fnjeuh.jpg"
       image25="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589732511/IMG_3915_oelxz6.jpg"
       image26="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589732515/IMG_4012-2_rocpcn.jpg"
-      image1="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589732523/IMG_4017-2_bts4im.jpg"
-      image30="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403286/bllacc2edit_rji4lh.jpg"
-      image28="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403294/bllacc6edit_go82rh.jpg"
-      image27="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403375/bllaccedit_j1ybfn.jpg"
-      image31="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403501/Soundz_y5ztx6.jpg"
-      image20="https://res.cloudinary.com/areoladaniel-com/image/upload/v1593794284/GatsbyPortfolio/optimized/cover6_d3mwbw.jpg"
+      image27="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403294/bllacc6edit_go82rh.jpg"
+      image29="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403375/bllaccedit_j1ybfn.jpg"
+      image30="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403305/DALU_qik4cm.jpg"
+      image31="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403286/bllacc2edit_rji4lh.jpg"
+      image32="https://res.cloudinary.com/areoladaniel-com/image/upload/v1589403501/Soundz_y5ztx6.jpg"
     />
+    ;
     <Footer />
   </div>
 );
