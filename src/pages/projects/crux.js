90fe2eb93970eabd824019bbef1aff4a7201ef32
lifeofdaniell <areoladanielopeyemi@gmail.com>
2020-07-09 14:34:19 +0100

    add more projects

--- src/pages/projects/crux.js
+++ src/pages/projects/crux.js
@@ -27,21 +27,18 @@ const Crux = () => (
       "
       description2="Even though they have an operational website, they found the use of Instagram to sell and advertise their products way easier!
       In that light, I designed an app for the online fashion store to help sell their products and not rely on the use of social media."
+      links="View full design on Behance"
+      link="https://www.behance.net/gallery/98306165/CRUX-Fashion-Store-App"
     />
 
     <ImageRow
-      column="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594300072/crux/1_etiuut.jpg"
-      column2="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297741/crux/2_qefdpb.jpg"
-      image1="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297748/crux/3_vjnjto.jpg"
-      image2="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297755/crux/4_hpwdnz.gif"
-      image3="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297748/crux/5_mxyfp1.jpg"
-      image4=""
+      image1="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594300072/crux/1_etiuut.jpg"
+      image2="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297748/crux/3_vjnjto.jpg"
+      image3="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297755/crux/4_hpwdnz.gif"
+      image4="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297748/crux/5_mxyfp1.jpg"
       image5="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297742/crux/7_ktskej.jpg"
-      image6="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297742/crux/8_h1zdpc.jpg"
-      image7="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297744/crux/9_h08mx2.jpg"
-      image8="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297742/crux/10_aas845.jpg"
-      image9="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297746/crux/11_lhbqzi.jpg"
-      image10="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297745/crux/12_mwxe1s.jpg"
+      image6="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297744/crux/9_h08mx2.jpg"
+      image7="https://res.cloudinary.com/areoladaniel-com/image/upload/v1594297742/crux/10_aas845.jpg"
     />
     <Footer />
   </div>
